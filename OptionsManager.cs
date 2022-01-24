84d2f4f2b87195fd0506832eb8769d54a6c6ca13
deyan <deyanstoev_2003@abv.bg>
2022-01-24 22:17:49 +0200

    comit1

--- OptionsManager.cs
+++ OptionsManager.cs
@@ -2,17 +2,22 @@
 
 namespace GitExercise
 {
-    public static class OptionsManager
-    {
-        public static string[] OptionsList = {
-            "a - Add"
-        };
+	public static class OptionsManager
+	{
+		public static string[] OptionsList = {
+			"a - Add"," s-Substract"
+		};
 
         public static void Add(double a, double b)
         {
             Console.WriteLine($"{a} + {b} = {a + b}");
         }
+		public static void Subtract(double a, double b)
+		{
+			Console.WriteLine($"{a} - {b} = {a - b}");
+		}
 
-       
-    }
+
+
+	}
 }
